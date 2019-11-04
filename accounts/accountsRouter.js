072f5e5b20f156c0d773a1012cf0f3a4392bca58
Surfsol <surfsol@protonamil.com>
2019-11-04 18:00:35 -0300

    Update accountsRouter.js

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     //select * from posts where id = req.params.id
     db.select('*').from('accounts').where('id', '=', req.params.id)
+    .first() //give you first item of array, only 1 item, cause 1 id
     .then(account => {
         res.status(200).json(account)
     }).catch(error => {
@@ -26,15 +27,44 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    const postData = req.body;
+    // {
+    //     "name": "Wine",
+    //     "budget": 556789
+    //   }
+    //validate the data before saving.
+    //sqlite always get id back, need to include id
+    db('accounts').insert(postData, 'id')
+    .then(ids => {
+        res.status(200).json(ids)
+    }).catch(error => {
+        res.status(500).json(error)
+    })
 
 });
 
 router.put('/:id', (req, res) => {
-
+    const id = req.params.id
+    const changes = req.body
+    db('accounts')
+    .where({id: req.params.id})
+    .update(changes)
+    .then(count => {
+        res.status(200).json(count)
+    }).catch(error => {
+        res.status(500).json(error)
+    })
 });
 
 router.delete('/:id', (req, res) => {
-
+    db('accounts')
+    .where({id: req.params.id})
+    .del()
+    .then(count => {
+        res.status(200).json(count)
+    }).catch(error => {
+        res.status(500).json(error)
+    })
 });
 
 module.exports = router;
